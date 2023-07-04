2085ddc3c98046d8623fcd700c0b5372068b2688
Rahul Singh <137313478+Rahul-1337@users.noreply.github.com>
2023-07-04 16:50:55 +0530

    Add files via upload

---
+++ Data-cleaning-and-analysis-using-SQL.sql
@@ -0,0 +1,164 @@
+--Basic exploration of data
+SELECT ride_id,
+        started_at,
+        ended_at,
+        ride_length,
+        day_of_week, 
+        start_station_name,
+        end_station_name,
+        member_casual
+ FROM `my-project-1-373317.cyclistic.QUARTER1` 
+ ORDER BY ride_id DESC
+
+
+ --Creating Quarter
+ CREATE TABLE cyclistic.QUARTER1 AS
+SELECT* FROM `my-project-1-373317.cyclistic.JAN`
+UNION ALL
+SELECT * FROM `my-project-1-373317.cyclistic.FEB`
+UNION ALL
+SELECT * FROM `my-project-1-373317.cyclistic.MAR`
+
+
+--Finding median ride
+SELECT
+        member_casual, 
+        day_of_week AS mode_day_of_week # Top number of day_of_week
+FROM 
+        (
+        SELECT
+                DISTINCT member_casual, day_of_week, ROW_NUMBER() OVER (PARTITION BY member_casual ORDER BY COUNT(day_of_week) DESC) rn
+        FROM
+                `my-project-1-373317.cyclistic.QUARTER1`
+        GROUP BY
+                member_casual, day_of_week
+        )
+WHERE
+        rn = 1
+ORDER BY
+        member_casual DESC LIMIT 100
+
+
+--Finding total number of trips
+SELECT 
+        TotalTrips,
+        TotalMemberTrips,
+        TotalCasualTrips,
+        ROUND(TotalMemberTrips/TotalTrips,2)*100 AS MemberPercentage,
+        ROUND(TotalCasualTrips/TotalTrips,2)*100 AS CasualPercentage
+FROM 
+        (
+        SELECT
+                COUNT(ride_id) AS TotalTrips,
+                COUNTIF(member_casual = 'member') AS TotalMemberTrips,
+                COUNTIF(member_casual = 'casual') AS TotalCasualTrips,
+        FROM
+                `my-project-1-373317.cyclistic.QUARTER1`
+        )
+        
+
+--avg ride differnce
+SELECT(
+  SELECT AVG(TIME_DIFF(ride_length,TIME'00:00:00',SECOND))
+  FROM `my-project-1-373317.cyclistic.QUARTER1`
+  
+) AS AvgRideLength_Overall,
+(SELECT AVG(TIME_DIFF(ride_length,TIME'00:00:00',
+SECOND))
+FROM `my-project-1-373317.cyclistic.QUARTER1`
+WHERE member_casual='member'
+) AS AvgRideLength_Member,
+(
+  SELECT AVG(TIME_DIFF(ride_length,TIME'00:00:00',
+SECOND))
+FROM `my-project-1-373317.cyclistic.QUARTER1`
+WHERE member_casual='casual'
+) AS AvgRideLength_Casual
+
+
+--max number of rides
+SELECT
+        member_casual,
+        MAX(ride_length) AS ride_length_MAX
+FROM `my-project-1-373317.cyclistic.QUARTER1`
+GROUP BY 
+        member_casual
+ORDER BY 
+        ride_length_MAX DESC
+LIMIT 100
+
+
+--median ride length for annual members
+SELECT
+        DISTINCT median_ride_length,
+        member_casual,
+        day_of_week
+FROM 
+        (
+        SELECT 
+                ride_id,
+                member_casual,
+                day_of_week,
+                ride_length,
+                PERCENTILE_DISC(ride_length, 0.5 IGNORE NULLS) OVER(PARTITION BY day_of_week) AS  median_ride_length
+        FROM `my-project-1-373317.cyclistic.QUARTER1`
+        WHERE
+                member_casual = 'member'
+        )
+ORDER BY 
+        median_ride_length DESC LIMIT 7
+
+
+--median ride length for casual members
+SELECT
+        DISTINCT median_ride_length,
+        member_casual,
+        day_of_week
+FROM 
+        (
+        SELECT 
+                ride_id,
+                member_casual,
+                day_of_week,
+                ride_length,
+                PERCENTILE_DISC(ride_length, 0.5 IGNORE NULLS) OVER(PARTITION BY day_of_week) AS  median_ride_length
+        FROM 
+                `my-project-1-373317.cyclistic.QUARTER1`
+        WHERE
+                member_casual = 'casual'
+        )
+ORDER BY 
+        median_ride_length DESC LIMIT 7
+
+--finding most popular start stations
+SELECT 
+        DISTINCT start_station_name,
+        SUM(
+            CASE WHEN ride_id = ride_id AND start_station_name = start_station_name THEN 1 ELSE 0 END
+            ) AS total,
+        SUM(
+            CASE WHEN member_casual = 'member' AND start_station_name = start_station_name THEN 1 ELSE 0 END
+            ) AS member,
+        SUM(
+            CASE WHEN member_casual = 'casual' AND start_station_name = start_station_name THEN 1 ELSE 0 END
+            ) AS casual
+FROM  `my-project-1-373317.cyclistic.QUARTER1`
+GROUP BY 
+        start_station_name
+ORDER BY 
+        total DESC
+
+
+--total rides per day
+SELECT  
+        day_of_week,
+        COUNT(DISTINCT ride_id) AS total_trips,
+        SUM(CASE WHEN member_casual = 'member' THEN 1 ELSE 0 END) AS MemberTrips,
+        SUM(CASE WHEN member_casual = 'casual' THEN 1 ELSE 0 END) AS CasualTrips
+FROM 
+        `my-project-1-373317.cyclistic.QUARTER1`
+GROUP BY 
+        1
+ORDER BY 
+        total_trips DESC LIMIT 7
+        
---
+++ cyclistic-visualisation-in-R.R
@@ -0,0 +1,77 @@
+library(dplyr)
+library(tidyverse)
+install.packages("skimr")
+library(skimr)
+library(janitor)
+library(readr)
+library(lubridate)
+
+#importing all 12 months of data
+trip_jan <- read_csv("D:/case study 1 data/ready for SQL/202201cs1.csv")
+trip_feb <- read_csv("D:/case study 1 data/ready for SQL/202202 cs1 - 202202-divvy-tripdata.csv")
+trip_mar <- read_csv("D:/case study 1 data/ready for SQL/202203 cs1 - 202203-divvy-tripdata.csv")
+trip_apr <- read_csv("D:/case study 1 data/ready for SQL/202204 cs1 - 202204-divvy-tripdata.csv")
+trip_may <- read_csv("D:/case study 1 data/ready for SQL/202205 cs1 - 202205-divvy-tripdata_1.csv")
+trip_june <-read_csv("D:/case study 1 data/ready for SQL/202206 cs1 - 202206-divvy-tripdata_1.csv")
+trip_july <-read_csv("D:/case study 1 data/ready for SQL/202207 cs1 - 202207-divvy-tripdata_1.csv")
+trip_aug <- read_csv("D:/case study 1 data/ready for SQL/202208 cs1 - Sheet1.csv")
+trip_sep <- read_csv("D:/case study 1 data/ready for SQL/202209 cs1.csv")
+trip_oct <- read_csv("D:/case study 1 data/ready for SQL/202210 cs1 - 202210-divvy-tripdata_1.csv")
+trip_nov <- read_csv("D:/case study 1 data/ready for SQL/202211 cs1 - Sheet1.csv")
+trip_dec <- read_csv("D:/case study 1 data/ready for SQL/202212 cs1 - 202212-divvy-tripdata_1.csv")
+
+#combining all 12 months of data in a single dataset called trip_2022
+trip_2022 <- rbind(trip_jan,trip_feb,trip_mar,trip_apr,trip_may,trip_june,trip_july,trip_aug,trip_sep,trip_oct,trip_nov,trip_dec)
+
+#Visualising Total Rides taken by Members & Casual Riders
+trip_2022 %>% 
+  group_by(member_casual) %>% 
+  summarise(ride_count=length(ride_id)) %>% 
+  ggplot()+geom_col(mapping = aes(x=member_casual,y=ride_count, fill=member_casual,),show.legend = "false") + 
+  labs(title = "Total No of Rides")
+
+#Visualising the Days of the Week with No. of Rides taken by Riders
+trip_2022 %>% 
+  group_by(member_casual, day_of_week) %>% 
+  summarise(number_of_rides=n(), .groups = "drop") %>% 
+  ggplot(aes(x = day_of_week, y = number_of_rides, fill = member_casual)) +
+  labs(title = "Total Rides vs.Day of The Week") +
+  geom_col(width = 0.5, position = position_dodge(width = 0.5)) +
+  scale_y_continuous(labels = function(x) format(x,scientific = FALSE))
+
+#finding mean, median, max and min of ride_length
+trip_2022 %>% 
+  group_by(member_casual) %>% summarise(average_ride_length=mean(ride_length),median_ride_length=median(ride_length),max_ride_length=max(ride_length),min_ride_length=min(ride_length))
+
+#calculating total number of rides
+trip_2022 %>% 
+  group_by(member_casual) %>% 
+  summarise(ride_count=length(ride_id))
+
+#Calculating average ride length and no. of rides as per day of the week.
+trip_2022 %>% 
+  group_by(member_casual,day_of_week) %>% 
+  summarise(number_of_rides=n(),average_ride_length=mean(ride_length),.groups = "drop")
+
+#Visualizing Average Ride by Day of the Week
+trip_2022 %>% 
+  group_by(member_casual,day_of_week) %>% 
+  summarise(average_ride_length=mean(ride_length), .groups = "drop") %>% 
+  ggplot(aes(x = day_of_week, y = average_ride_length, fill = member_casual)) +
+  geom_col(width = 0.5, position = position_dodge(width = 0.5)) +
+  labs(title = "Average ride length vs. Day of The Week")
+
+#visualizing and Comparing Casual and Member Rides by Distance
+trip_2022 %>% 
+  group_by(member_casual) %>%
+  summarise(average_ride_distance = mean(ride_length)) %>%
+  ggplot() + geom_col(mapping = aes(x = member_casual, y = average_ride_distance, fill = member_casual), show.legend = FALSE) +
+  labs(title = "Average Distance Travelled")
+
+#Comparison of Total rides with the Type of Ride
+trip_2022 %>% 
+  group_by(member_casual, rideable_type) %>%
+  summarise(number_of_rides = n(), .groups = "drop") %>%
+  ggplot() + geom_col(mapping = aes(x = rideable_type, y = number_of_rides, fill = member_casual), show.legend = TRUE) +
+  
+  labs(title = "Total no. of Rides vs. Ride Type")
